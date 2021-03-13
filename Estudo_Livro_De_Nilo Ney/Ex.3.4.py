20ffe6120fb5a2cbee5b8fd6d16b52193a261c3c
Fábio Amaral Rodrigues <famaral_r07@icloud.com>
2021-03-13 11:29:19 -0300

    Update Ex.3.4.py

--- Estudo_Livro_De_Nilo Ney/Ex.3.4.py
+++ Estudo_Livro_De_Nilo Ney/Ex.3.4.py
@@ -1,18 +1,23 @@
 # Exercício 3.4 - Calcular se paga imposto ou não, referência R$1200.00
 
-#sal = float(input('Informe seu salário'))
+sal = float(input('Informe seu salário'))
 
-#if sal <=1200
-    #print('Voce nao paga imposto')
+imposto = not sal<1200
+if imposto == True:
+    print('Você irá pagar imposto de renda =/')
+if imposto == False:
+    print('Você não irá pagar imposto de renda =)')
 
 
 # Exercício 3.6
 
-m1 = input('Informe ume nota até 10')
-m2= input('Informe ume nota até 10')
-m3 = input('Informe ume nota até 10')
+m1 = int(input('Informe ume nota até 10: '))
+m2= int(input('Informe ume nota até 10: '))
+m3 = int(input('Informe ume nota até 10: '))
 md = 7
 
 aprovado = m1>=md and m2>=md and m3>=md
-if aprovado == True 
+if aprovado == True:
     print('APROVADO')
+if aprovado == False:
+    print('REPROVADO')
