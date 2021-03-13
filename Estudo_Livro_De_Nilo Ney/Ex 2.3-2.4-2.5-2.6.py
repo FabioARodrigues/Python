a2c016e79de8641ffb9ecc3f464c5a85e0183c4e
Fábio Amaral Rodrigues <famaral_r07@icloud.com>
2021-03-13 11:15:07 -0300

    new

---
+++ Estudo_Livro_De_Nilo Ney/Ex 2.3-2.4-2.5-2.6.py
@@ -0,0 +1,25 @@
+# Exercício 2.3
+
+nome = input("olá! Informe seu nome:")
+print(nome)
+
+# Exercício 2.4
+
+a = 3
+b = 5
+print(2*a + 3*b)
+
+# Exercício 2.5
+
+a = 3
+b = 7
+c = 4
+print(a+b+c)
+
+# Exercício 2.6
+
+salário = 750
+aumento = 0.15
+sal_real = (salário+(salário*aumento))
+print(sal_real)
+
---
+++ Estudo_Livro_De_Nilo Ney/Ex.3.4.py
@@ -0,0 +1,18 @@
+# Exercício 3.4 - Calcular se paga imposto ou não, referência R$1200.00
+
+#sal = float(input('Informe seu salário'))
+
+#if sal <=1200
+    #print('Voce nao paga imposto')
+
+
+# Exercício 3.6
+
+m1 = input('Informe ume nota até 10')
+m2= input('Informe ume nota até 10')
+m3 = input('Informe ume nota até 10')
+md = 7
+
+aprovado = m1>=md and m2>=md and m3>=md
+if aprovado == True 
+    print('APROVADO')
